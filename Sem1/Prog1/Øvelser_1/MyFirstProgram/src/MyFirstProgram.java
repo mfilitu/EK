5dad33b6f88abc7748f703ff6fa751cd13a2bede
Mateusz Andrzej Filipowski <70143907+mfilitu@users.noreply.github.com>
2026-01-28 13:19:32 +0100

    files

---
+++ Sem1/Prog1/Øvelser_1/MyFirstProgram/src/MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hey there!");
+        System.out.print("xD \n");
+        System.out.println("test \t test \\");
+
+    }
+}
