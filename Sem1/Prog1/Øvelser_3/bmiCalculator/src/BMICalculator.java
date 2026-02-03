8da916695267470ec5522a4cbcbf3a97de84a3aa
Mateusz Andrzej Filipowski <70143907+mfilitu@users.noreply.github.com>
2026-02-03 17:56:54 +0100

    Øvelser

--- Sem1/Prog1/Øvelser_3/bmiCalculator/src/BMICalculator.java
+++ Sem1/Prog1/Øvelser_3/bmiCalculator/src/BMICalculator.java
@@ -1,20 +1,24 @@
 public class BMICalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("BMI: " + calculateBMI(86, 1.80));
         System.out.println("Under normalvægt: " + isUnderNormalWeigh(calculateBMI(86, 1.80)));
         System.out.println("Normalvægt: " + isNormalWeight(calculateBMI(86, 1.80)));
         System.out.println("Overvægt: " + isOverweight(calculateBMI(86, 1.80)));
     }
-    public static double calculateBMI(int kg, double height){
-        return kg / (height*height);
+
+    public static double calculateBMI(int kg, double height) {
+        return kg / (height * height);
     }
-    public static boolean isUnderNormalWeigh(double bmi){
+
+    public static boolean isUnderNormalWeigh(double bmi) {
         return bmi < 18.5;
     }
-    public static boolean isNormalWeight(double bmi){
+
+    public static boolean isNormalWeight(double bmi) {
         return bmi >= 18.5 && bmi < 25;
     }
-    public static boolean isOverweight(double bmi){
+
+    public static boolean isOverweight(double bmi) {
         return bmi > 25;
     }
 }
