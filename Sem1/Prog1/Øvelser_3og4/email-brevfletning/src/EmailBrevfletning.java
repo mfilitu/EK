25afcdaccd602c1e853c0da18c6bad6ffd271fbd
Mateusz Andrzej Filipowski <70143907+mfilitu@users.noreply.github.com>
2026-02-04 14:18:54 +0100

    øvelser

--- Sem1/Prog1/Øvelser_3og4/email-brevfletning/src/EmailBrevfletning.java
+++ Sem1/Prog1/Øvelser_3og4/email-brevfletning/src/EmailBrevfletning.java
@@ -1,2 +1,13 @@
 public class EmailBrevfletning {
+    public static void main(String[] args){
+        emailGen("Mateusz", "mafi1000@stud.ek.dk", "Mor", "mor@mother.dk","Se hvor sej jeg er", "Hej Mor\nJeg er mega sej B).\nMvh Mateusz");
+    }
+    public static void emailGen(String recipient, String rEmail, String sender, String sEmail, String subject, String body){
+        System.out.printf(
+                "From: %s <%s>%n" +
+                "To: %s <%s>%n" +
+                "Subject: %s%n%n" +
+                "%s", recipient, rEmail, sender, sEmail, subject, body);
+
+    }
 }
